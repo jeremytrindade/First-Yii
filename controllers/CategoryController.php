76ce540633fe61156ef8fbb99984ed5e38de8fd3
Jérémy Da Trindade <admin@MacBook-Pro-de-Jeremy.local>
2018-08-06 12:04:03 +0200

    Create Models and Category Actions

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -2,15 +2,56 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\web\Response;
+use yii\web\Controller;
+use app\models\Category;
+use yii\data\Pagination;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
 class CategoryController extends \yii\web\Controller
 {
-    public function actionIndex()
-    {
-        return $this->render('index');
+    public function actionIndex(){
+        // Create Query
+        $query = Category::find();
+        
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount'=>$query->count()
+        ]);
+
+        $categories = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'categories' => $categories,
+            'pagination' => $pagination
+        ]);
+
     }
 
     public function actionCreate()
     {
-        return $this->render('create');
+        $category = new Category();
+
+        if ($category->load(Yii::$app->request->post())) {
+            // Validation
+            if ($category->validate()) {
+                // Save Record
+                $category->save();
+
+                // Send Message
+                Yii::$app->getSession()->setFlash('success','Category Added');
+                // form inputs are valid, do something here
+                return $this->redirect('index.php?r=category');
+            }
+        }
+
+        return $this->render('create', [
+            'category' => $category
+        ]);
     }
 }
--- views/category/index.php
+++ views/category/index.php
@@ -1,9 +1,16 @@
 <?php
 /* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
 ?>
-<h1>category/index</h1>
+<h2 class="page-header">category <a href="/index.php?r=category/create" class="btn btn-primary pull-right">Create</a></h2>
+<?php if(null !== Yii::$app->session->getFlash('success')):?>
+    <div class="alert alert-success"><?php Yii::$app->session->getFlash('success'); ?></div>
+<?php endif;?>
+<ul class="list-group">
+    <?php foreach($categories as $category):?>
+        <li class="list-group-item"><a href="/index.php?r=job&category=<?=$category->id;?>"><?php echo $category->name;?></a></li>
+<?php endforeach;?>
+</ul>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<?= LinkPager::widget(['pagination' => $pagination]);?>
